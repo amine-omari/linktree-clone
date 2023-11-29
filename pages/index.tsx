d4d25cc82d35c39ee803e3b5127c9c9683cedf9f
amine alaoui <omari1amineco@gmail.com>
2023-11-29 18:47:46 +0100

    feat: add the title of the image

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import data from "../data.json";
 
 export default function Home() {
   return (
-    <div className="flex justify-center mx-auto w-full mt-16">
+    <div className="flex flex-col items-center justify-center mx-auto w-full mt-16">
       <Image className="rounded-full" src={data.avatar} alt={data.name} width={96} height={96} />
+      <h1 className="font-bold mt-4 text-xl">{data.name}</h1>
     </div>
   );
 }
