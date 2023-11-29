0b20b75f375d71ed15ba5532615115400fa316bf
amine alaoui <omari1amineco@gmail.com>
2023-11-29 18:39:50 +0100

    feat: add a Next Image in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-
+import Image from "next/image";
+import data from "../data.json";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center mx-auto w-full mt-16">
+      <Image className="rounded-full" src={data.avatar} alt={data.name} width={96} height={96} />
     </div>
   );
 }
