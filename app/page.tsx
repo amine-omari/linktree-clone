c75a39959e78c106d39271e932906ec0f966bbe6
amine alaoui <omari1amineco@gmail.com>
2023-11-30 21:33:14 +0100

    fix: page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -75,11 +75,13 @@ interface Data {
   links: Link[];
   socials: Social[];
 }
+
 interface Link {
   href: string;
   title: string;
   image?: string;
 }
+
 interface Social {
   href: string;
   title: string;
