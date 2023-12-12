0e0f90bfa188a314f6991dfadde87c3741a16e06
amine alaoui <omari1amineco@gmail.com>
2023-12-12 18:56:16 +0100

    fix title of the page

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Linktree</title>
+      <title>Linktree clone</title>
       <meta name="descrption" content="Next.js + Tailwind.css" />
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
